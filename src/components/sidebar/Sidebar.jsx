e7a53bd1136d1e47b2c4cda893c5826a0e5b0275
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-08-16 02:35:35 +0330

    fixed some bugs and add links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,6 +11,9 @@ import { MailOutline } from '@mui/icons-material'
 import { DynamicFeed } from '@mui/icons-material'
 import { ChatBubbleOutline } from '@mui/icons-material'
 import { MessageOutlined } from '@mui/icons-material'
+import { WorkOutline } from '@mui/icons-material'
+import { Report } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   return (
@@ -19,7 +22,7 @@ export default function Sidebar() {
       <div className="sidebarMenu">
         <h3 className="sidebarTitle">Dashboard</h3>
         <ul className="sidebarList">
-          <li className="sidebarList_item active"><LineStyle className='sidebarIcon'/> Home</li>
+         <Link to="/" className='link'> <li className="sidebarList_item active"><LineStyle className='sidebarIcon'/> Home</li></Link>
           <li className="sidebarList_item"><Timeline className='sidebarIcon'/> Analytics</li>
           <li className="sidebarList_item"><TrendingUp className='sidebarIcon'/> Sales</li>
         </ul>
@@ -28,9 +31,9 @@ export default function Sidebar() {
       <div className="sidebarMenu">
         <h3 className="sidebarTitle">Quick Menu</h3>
         <ul className="sidebarList">
-          <li className="sidebarList_item "><PermIdentity className='sidebarIcon'/> Users</li>
-          <li className="sidebarList_item"><Storefront className='sidebarIcon'/> New User</li>
-          <li className="sidebarList_item"><AttachMoney className='sidebarIcon'/> Products</li>
+        <Link to="/users"  className='link'><li className="sidebarList_item "><PermIdentity className='sidebarIcon'/> Users</li></Link>
+        <Link to="/newUser"  className='link'> <li className="sidebarList_item"><Storefront className='sidebarIcon'/> New User</li></Link>
+        <Link to="/products"  className='link'><li className="sidebarList_item"><AttachMoney className='sidebarIcon'/> Products</li></Link>
           <li className="sidebarList_item"><BarChart className='sidebarIcon'/> Transaction</li>
           <li className="sidebarList_item"><MailOutline className='sidebarIcon'/> Reports</li>
         </ul>
@@ -42,6 +45,15 @@ export default function Sidebar() {
           <li className="sidebarList_item "><DynamicFeed className='sidebarIcon'/> Mail</li>
           <li className="sidebarList_item"><ChatBubbleOutline className='sidebarIcon'/> Feedback</li>
           <li className="sidebarList_item"><MessageOutlined className='sidebarIcon'/> Message</li>
+        </ul>
+      </div>
+
+      <div className="sidebarMenu">
+        <h3 className="sidebarTitle">Staff</h3>
+        <ul className="sidebarList">
+          <li className="sidebarList_item "><WorkOutline className='sidebarIcon'/> Manage</li>
+          <li className="sidebarList_item"><Timeline className='sidebarIcon'/> Analytics</li>
+          <li className="sidebarList_item"><Report className='sidebarIcon'/> Report</li>
         </ul>
       </div>
 
